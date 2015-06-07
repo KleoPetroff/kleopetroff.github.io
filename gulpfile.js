80b5447cef3b444e121725ef9025f659e4341fa9
KleoPetroff <kleopetroff@gmail.com>
2015-06-07 23:01:07 +0300

    Add gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,22 @@
-/**
- * Created by Kleo on 6/4/2015.
- */
+var gulp    = require('gulp'),
+    minify  = require('gulp-minify-css'),
+    concat  = require('gulp-concat-css'),
+    color   = require('cli-color'),
+    plubmer = require('gulp-plumber');
 
+
+// Minify and Concatenate all CSS files
+gulp.task('css', function() {
+    gulp.src('css/*.css')
+        .pipe(plubmer())
+        .pipe(concat('bundle.min.css'))
+        .pipe(minify())
+        .pipe(gulp.dest('assets/css'));
+    console.log(color.bold.blue.underline('Concatenation and Minification Successful :)'));
+});
+
+
+// Watching for changes in all css files
+gulp.task('watch', function() {
+    gulp.watch('css/*.css', ['css']);
+});
