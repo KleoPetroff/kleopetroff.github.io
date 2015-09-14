ff8be0e09f93da65a7d0d5a28f621617f88045ae
KleoPetroff <kleopetroff@gmail.com>
2015-09-15 00:49:49 +0300

    add gulp-sass plugin and remove old css gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,18 @@
 var gulp    = require('gulp'),
-    minify  = require('gulp-minify-css'),
-    concat  = require('gulp-concat-css'),
     color   = require('cli-color'),
-    plubmer = require('gulp-plumber');
-
+    plubmer = require('gulp-plumber'),
+    sass    = require('gulp-sass');
 
 // Minify and Concatenate all CSS files
-gulp.task('css', function() {
-    gulp.src('css/*.css')
-        .pipe(plubmer())
-        .pipe(concat('styles.min.css'))
-        .pipe(minify())
-        .pipe(gulp.dest('assets/css'));
-    console.log(color.bold.blue.underline('Concatenation and Minification Successful :)'));
+gulp.task('sass', function() {
+        gulp.src('assets/_scss/**/**.scss')
+            .pipe(sass({
+                outputStyle: 'compressed'
+            }).on('error', sass.logError))
+            .pipe(gulp.dest('assets/css'));
 });
-
 
 // Watching for changes in all css files
 gulp.task('watch', function() {
-    gulp.watch('css/*.css', ['css']);
+    gulp.watch('assets/_scss/**/*.scss', ['sass']);
 });
