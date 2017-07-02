e68c20044d6c4cef9788556bdc4621a056d8ec9f
KleoPetroff <kleopetroff@gmail.com>
2017-07-02 12:27:59 +0300

    add plumber to sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-var gulp    = require('gulp'),
-    color   = require('cli-color'),
-    plubmer = require('gulp-plumber'),
-    sass    = require('gulp-sass');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
 
 // Minify and Concatenate all CSS files
-gulp.task('sass', function() {
-        gulp.src('assets/_scss/**/**.scss')
-            .pipe(sass({
-                outputStyle: 'compressed'
-            }).on('error', sass.logError))
-            .pipe(gulp.dest('assets/css'));
+gulp.task('sass', function () {
+  gulp.src('assets/_scss/**/**.scss')
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('assets/css'));
 });
 
 // Watching for changes in all css files
-gulp.task('watch', function() {
-    gulp.watch('assets/_scss/**/*.scss', ['sass']);
+gulp.task('watch', function () {
+  gulp.watch('assets/_scss/**/*.scss', ['sass']);
 });
