f57fca02e5dd846ad4de1c1c16fab7630da31aaf
KleoPetroff <kleopetroff@gmail.com>
2018-12-30 17:02:17 +0200

    adjust the gulp tasks to the new v4 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,18 @@ const plumber = require('gulp-plumber');
 const sass = require('gulp-sass');
 
 // Minify and Concatenate all CSS files
-gulp.task('sass', function () {
+gulp.task('sass', function (done) {
   gulp.src('assets/_scss/**/**.scss')
     .pipe(plumber())
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(gulp.dest('assets/css'));
+
+  done();
 });
 
 // Watching for changes in all css files
 gulp.task('watch', function () {
-  gulp.watch('assets/_scss/**/*.scss', ['sass']);
+  gulp.watch('assets/_scss/**/*.scss', gulp.series('sass'));
 });
