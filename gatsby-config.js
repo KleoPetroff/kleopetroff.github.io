668828806ff7e31af8370642f0b29e9141d07670
KleoPetroff <kleopetroff@gmail.com>
2019-08-12 21:21:02 +0300

    dependency: setup dmx

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,14 @@ module.exports = {
         ]
       }
     },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.tsx')
+        }
+      }
+    },
     `gatsby-plugin-typescript`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
